355951f5291f479edfa25585cb8addc955d029fc
vadim <tank_vadim@inbox.ru>
2022-04-23 11:14:14 +0500

    Добавьте файлы проекта.

---
+++ WindowsFormsApp6/DataBase.cs
@@ -0,0 +1,109 @@
+﻿using MySql.Data.MySqlClient;
+using Org.BouncyCastle.Crypto.Tls;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp6
+{
+    class DataBase
+    {
+        public bool ExecuteCommand(MySqlCommand command)
+        {
+            bool bl = true;
+            try
+            {
+                Variables.Connection.Open();
+                command.ExecuteNonQuery();
+            }
+            catch (Exception x) { MessageBox.Show(x.ToString()); bl = false; }
+            if (Variables.Connection.State == ConnectionState.Open)
+                Variables.Connection.Close();
+            return bl;
+        }
+        public void FillConnection(string server, string port, string database, string username, string password)
+        {
+            Variables.Connection = new MySqlConnection(@"Server = " + server + "; Port = " + port + "; DataBase = '" + database + "'; user ID = '" + username + "'; Password = '" + password + "'; CharSet=utf8 ");
+        }
+        public bool TestConnection()
+        {
+            bool success = true;
+            try
+            {
+                Variables.Connection.Open();
+            }
+            catch
+            {
+                MessageBox.Show("Подключение не удалось.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                success = false;
+            }
+            if (Variables.Connection.State == ConnectionState.Open)
+                Variables.Connection.Close();
+            return success;
+        }
+        public void SaveConnection(string server, string port, string database, string username, string password)
+        {
+            if(!(Directory.Exists(Path.GetDirectoryName(Variables.filepath))))
+            {
+                Directory.CreateDirectory(Path.GetDirectoryName(Variables.filepath));
+                using (FileStream fs = File.Create(Variables.filepath)) { }
+            }
+            else if(!(File.Exists(Variables.filepath)))
+                using (FileStream fs = File.Create(Variables.filepath)) { }
+            try
+            {
+                using(FileStream fs= File.Open(Variables.filepath, FileMode.Open)) { fs.SetLength(0); }
+                using (StreamWriter sw = new StreamWriter(Variables.filepath))
+                {
+                    sw.WriteLine(server);
+                    sw.WriteLine(port);
+                    sw.WriteLine(database);
+                    sw.WriteLine(username);
+                    sw.WriteLine(password);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Невозможно получить доступ к файлу", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        public void LoadConnection(TextBox server, TextBox port, TextBox database, TextBox username, TextBox password)
+        {
+            if (File.Exists(Variables.filepath))
+            {
+                try
+                {
+                    using (StreamReader sr = new StreamReader(Variables.filepath))
+                    {
+                        server.Text = sr.ReadLine();
+                        port.Text = sr.ReadLine();
+                        database.Text = sr.ReadLine();
+                        username.Text = sr.ReadLine();
+                        password.Text = sr.ReadLine();
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("Невозможно получить думтуп к файлу", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+                MessageBox.Show("Файл сохранения не найден.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+        public void OpenDirectory()
+        {
+            if (Directory.Exists(Path.GetDirectoryName(Variables.filepath)))
+            {
+                Process.Start(Path.GetDirectoryName(Variables.filepath));
+            }
+            else
+                MessageBox.Show("Файл сохранения не найден.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+}
---
+++ WindowsFormsApp6/TittleForm.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp6
+{
+    public partial class TittleForm : Form
+    {
+        ResizeHandler Sizer = new ResizeHandler();
+        public TittleForm()
+        {
+            InitializeComponent();
+            Sizer.Form = this;
+            Sizer.GroupBox = gbServer;
+        }
+        private void btSettings_Click(object sender, EventArgs e)
+        {
+            Sizer.TittleResize();
+        }
+        private void btExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        private void btStart_Click(object sender, EventArgs e)
+        {
+            DataBase DB = new DataBase();
+            DB.FillConnection(tbServer.Text, tbPort.Text, tbDB.Text, tbLogin.Text, tbPassword.Text);
+            if (DB.TestConnection())
+            {
+                AuthForm frm = new AuthForm();
+                Hide();
+                frm.ShowDialog();
+            }
+        }
+
+        private void btClear_Click(object sender, EventArgs e)
+        {
+            tbServer.Text = "88.205.135.82";
+            tbPort.Text = "7077";
+            tvDB.Text = "children_entertaiment";
+            tbLogin.Text = "entertain";
+            tbPassword.Text = "1234";
+        }
+
+        private void brload_Click(object sender, EventArgs e)
+        {
+            DataBase DB = new DataBase();
+            DB.LoadConnection(tbServer, tbPort, tbDB, tbLogin, tbPassword);
+        }
+
+        private void TitleForm_Load(object sender, EventArgs e)
+        {
+            if (File.Exists(Variables.filepath))
+                btLoad_Click(sender, e);
+        }
+
+        private void btOpenDir_Click(object sender, EventArgs e)
+        {
+            DataBase DB = new DataBase();
+            DB.OpenDirectory();
+        }
+
+        public bool AuthCheck(string login, string password)
+        {
+            DataTable result = Select("users", "*", "login='" + login + "'AND password'" + password + "'AND enabled = 1");
+            if (result.Rows.Count == 1)
+            {
+                Variables.User.Fill(Convert.ToInt32(result.Rows[0][0]), result.Rows[0][2].ToString() + " " + result.Rows[0][3].ToString() + " " + result.Rows[0][4].ToString(), login, result.Rows[0][1].ToString());
+                return true;
+            }
+            else
+            {
+                MessageBox.Show("Неверный логин или пароль", "Ошибка");
+                return false;
+            }
+        }
+
+        private void TittleForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
